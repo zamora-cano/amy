723bd9fc6ea1cf62175257fe161cd495d9c971ba
zamora-cano <angelzamoracano@gmail.com>
2023-12-04 15:10:06 -0600

    Agregar que cuando diga Amy pueda hacer acciones

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import time
 from clases.voz import SpeechModule, VoiceRecognitionModule 
-from clases.action import ChatBot
+from clases.ChatBot import ChatBot
 
 speech = SpeechModule()
 recognition = VoiceRecognitionModule()
+chatbot_instance = ChatBot()
 
 while True:
     text = recognition.recognize()
     
-    if text:
-        ChatBot_resultado = ChatBot.text(text)
-        print(text,"\n",ChatBot_resultado)
-        speech.talk(ChatBot_resultado)
+    if "Amy" in text:
+        mandar = "¿Que necesitas papu?"
+        print(text, "\n", mandar)
+        speech.talk(mandar)
+        
+        text = recognition.recognize()
+
+        if text:
+            # Usar la instancia de ChatBot creada previamente
+            ChatBot_resultado = chatbot_instance.text(input_text=text)
+            print(text, "\n", ChatBot_resultado)
+            speech.talk(ChatBot_resultado)
+
     time.sleep(1)
